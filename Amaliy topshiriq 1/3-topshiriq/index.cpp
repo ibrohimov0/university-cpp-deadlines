d95d28850b59560403f1bbefb3e5a8336359219c
Your Name <you@example.com>
2025-10-16 14:11:46 +0500

    third task completed

---
+++ Amaliy topshiriq 1/3-topshiriq/index.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a,b,y,x,z;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    cout<<"z=";
+    cin>>z;
+
+    if(z<1){
+        x=pow(z,3)+0.2;
+    }else{
+        x=z+log(z);
+    }
+
+    double first_step=2*a*pow(sin(pow(x,2)),5);
+    double second_step=pow(cos(pow(x,3)),2);
+    y=first_step+second_step-b*x;
+
+    cout<<"y="<<y;
+    return 0;
+}
